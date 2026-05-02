e6e9b8686a40070cfaf6b7e5fe60325890d8a688
azidhossainfahim25 <sazidhossainfahim25@gmail.com>
2026-05-03 03:06:35 +0600

    update api

--- src/lib/getTiles.js
+++ src/lib/getTiles.js
@@ -1,6 +1,6 @@
 export async function getTiles() {
   try {
-    const res = await fetch('/data.json', {
+    const res = await fetch('https://tiles-gallery-beryl.vercel.app/data.json', {
       cache: 'no-store',
     });
 
