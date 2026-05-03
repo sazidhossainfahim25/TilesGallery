e2812581b00168be71c7c2da899d90d153cedf39
azidhossainfahim25 <sazidhossainfahim25@gmail.com>
2026-05-03 15:49:46 +0600

    profile

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -7,11 +7,10 @@ const UserProfilePage = () => {
   const user = session?.user;
 
   return (
-    <div className="min-h-screen py-10 lg:py-16 flex items-center justify-center relative overflow-hidden bg-[url('/images/banner1.jpg')] bg-cover bg-center">
-      <div className="absolute inset-0 bg-black/60"></div>
+    <div className=" py-10 lg:py-10 flex items-center justify-center">
 
-      <div className="relative z-10 w-full max-w-[80%] sm:max-w-md lg:max-w-xl mx-auto">
-        <div className="bg-white/10 backdrop-blur-md rounded-xl border border-white/20 shadow-2xl overflow-hidden">
+      <div className="w-full max-w-[80%] sm:max-w-md lg:max-w-xl mx-auto">
+        <div className="bg-white  backdrop-blur-md rounded-xl border border-white/20 shadow-2xl overflow-hidden">
           <form className="p-6 sm:p-8 lg:p-10 flex flex-col items-center lg:gap-4 gap-1">
             <div className="avatar">
               <div className="w-24 h-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 overflow-hidden relative bg-white/10 flex items-center justify-center">
